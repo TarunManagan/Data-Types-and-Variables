1a5f7e372164a184368b4e7d65bf21575e613bd6
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 13:39:35 +0530

    Add files via upload

---
+++ Experiment_1_a.cpp
@@ -0,0 +1,14 @@
+/*  PRN:22070123118
+    Experiment no:1(a)
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!";
+    return 0;
+}
+
+/*  OUTPUT
+Hello World!
+*/
